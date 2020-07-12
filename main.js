0d0a677fb96fc11acc3c2a379fd4e457eb224b60
Sofia Ramon <sofiapaula96@gmail.com>
2020-07-11 21:23:15 -0300

    login y buscador notebook

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-var uno = 1234;
-console.log(uno);
+var login = document.querySelector(".buscador");
+login.onclick = function(){
+  document.querySelector(".buscadorbarra").style.display = "flex";
+
+}
+
+var section = document.querySelector("section");
+section.onclick = function(){
+  document.querySelector(".buscadorbarra").style.display = "none";
+
+}
