eb893c0b44b3feb8f2f13f61d58ed875cb3165e8
Sofia Ramon <sofiapaula96@gmail.com>
2020-07-14 21:10:13 -0300

    menu y carga

--- main.js
+++ main.js
@@ -7,5 +7,11 @@ login.onclick = function(){
 var section = document.querySelector("section");
 section.onclick = function(){
   document.querySelector(".buscadorbarra").style.display = "none";
+  document.querySelector(".phoneytablet-submenu").style.display = "none";
 
+}
+
+var menu = document.querySelector(".menuphoneboton");
+menu.onclick = function(){
+  document.querySelector(".phoneytablet-submenu").style.display = "block";
 }
