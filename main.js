a4f2b516bec1a1105d4439c5684e68ff31ff936a
Sofia Ramon <sofiapaula96@gmail.com>
2020-07-16 15:02:00 -0300

    menu y contacto

--- main.js
+++ main.js
@@ -7,11 +7,57 @@ login.onclick = function(){
 var section = document.querySelector("section");
 section.onclick = function(){
   document.querySelector(".buscadorbarra").style.display = "none";
-  document.querySelector(".phoneytablet-submenu").style.display = "none";
+  document.querySelector(".submenu-mobile").style.display = "none";
 
 }
 
-var menu = document.querySelector(".menuphoneboton");
+var menu = document.querySelector(".icono-menu");
+document.querySelector(".submenu-mobile").style.display = "none";
 menu.onclick = function(){
-  document.querySelector(".phoneytablet-submenu").style.display = "block";
+  var estado = document.querySelector(".submenu-mobile").style.display;
+  if (estado == "none") {
+    document.querySelector(".submenu-mobile").style.display = "block";
+  }
+  else {
+    document.querySelector(".submenu-mobile").style.display = "none";
+  }
+  };
+
+var buscadormobile = document.querySelector(".icono-lupa");
+document.querySelector(".buscadorbarra-mobile").style.display = "none";
+buscadormobile.onclick = function(){
+  var estado = document.querySelector(".buscadorbarra-mobile").style.display;
+  if (estado == "none") {
+    document.querySelector(".buscadorbarra-mobile").style.display = "flex";
+  }
+  else {
+    document.querySelector(".buscadorbarra-mobile").style.display = "none";
+  }
 }
+
+var productosmenu = document.getElementById("productos");
+productosmenu.querySelector("ul").style.display = "none"
+
+productosmenu.onclick = function submenu(){
+  var estado = productosmenu.querySelector("ul").style.display;
+  if (estado == "none") {
+    productosmenu.querySelector("ul").style.display = "block";
+  }
+  else {
+    productosmenu.querySelector("ul").style.display = "none";
+  }
+};
+
+
+var serviciosmenu = document.getElementById("servicios");
+serviciosmenu.querySelector("ul").style.display = "none"
+
+serviciosmenu.onclick = function submenu(){
+  var estado = serviciosmenu.querySelector("ul").style.display;
+  if (estado == "none") {
+    serviciosmenu.querySelector("ul").style.display = "block";
+  }
+  else {
+    serviciosmenu.querySelector("ul").style.display = "none";
+  }
+};
