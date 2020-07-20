50e68e3dc930cc9bd68844edcec885151c7a2c9c
Sofia Ramon <sofiapaula96@gmail.com>
2020-07-20 17:48:23 -0300

    adaptacion

--- resources/views/layouts/body_layout.blade.php
+++ resources/views/layouts/body_layout.blade.php
@@ -27,7 +27,7 @@
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    @if(Auth::user()->is_admin)
+    @if(Auth::user() && Auth::user()->is_admin)
     <link rel="stylesheet" type="text/css" href="/css/adminNavBar/component.css" />
     <script src="/js/adminNavBar/modernizr-2.6.2.min.js"></script>
     @endif
@@ -169,7 +169,7 @@
         <!-- End Nav Structure -->
     </div>
     @endif
-    @if(Auth::user()->is_admin)
+    @if(Auth::user() && Auth::user()->is_admin)
     <script src="/js/adminNavBar/polyfills.js"></script>
     <script src="/js/adminNavBar/demo1.js"></script>
     @endif
